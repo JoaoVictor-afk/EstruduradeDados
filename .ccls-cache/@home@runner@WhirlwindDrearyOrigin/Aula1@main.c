cdc3d178b54777b13fb9c989a77d7184a888396d
João Victor Peixoto de Souza <82658732+JoaoVictor-afk@users.noreply.github.com>
2023-02-17 12:10:53 +0000

    gerarpilha

--- .ccls-cache/@home@runner@WhirlwindDrearyOrigin/Aula1@main.c
+++ .ccls-cache/@home@runner@WhirlwindDrearyOrigin/Aula1@main.c
@@ -1,6 +1,8 @@
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 typedef struct no {
   int dado;
@@ -57,7 +59,7 @@ void exibir(Celula *topo) {
   }
   Celula *p;
   for (p = topo; p; p = p->prox) {
-    printf("%d\t", p->dado);
+    printf("%d\t\n", p->dado);
   }
 }
 
@@ -68,30 +70,40 @@ int localizado(int valor, Celula *topo) {
 
   for (p = topo; p; p = p->prox) {
     if (valor == p->dado)
+
       return 1;
   }
   return 0; // passou por toda a pilha e nao encontrou o valor
 }
+void gerarPilha(Celula *pilharand, int n) {
+  srand(time(NULL));
+  int i;
+  n = 100;
+  for (i = 0; n >= i; i++) {
+    pilharand = inserir(rand() % 100, pilharand);
+  }
+  return exibir(pilharand);
+}
 
 int main() {
-  Celula *pilha1 = NULL; // pilha1 é um ponteiro de Celula que vai apontar para
-                         // o ultimo elemento inserido
+  Celula *pilha1 = NULL; // pilha1 é um ponteiro de Celula que vai apontar
 
-  pilha1 = inserir(15, pilha1);
-  pilha1 = inserir(10, pilha1);
-  pilha1 = inserir(5, pilha1);
-  pilha1 = inserir(25, pilha1);
-  pilha1 = inserir(100, pilha1);
+  // pilha1 = inserir(12, pilha1);
+  // pilha1 = inserir(10, pilha1);
+  // pilha1 = inserir(5, pilha1);
+  // pilha1 = inserir(25, pilha1);
+  // pilha1 = inserir(100, pilha1);
+  // // o ultimo elemento inserido
+  // pilha1 = remover(pilha1);
 
-  pilha1 = remover(pilha1);
+  // printf("Endereco do ultimo elemento: %p\n", pilha1);
+  // printf("Valor do ultimo elemenbto  : %d\n", pilha1->dado);
 
-  printf("Endereco do ultimo elemento: %p\n", pilha1);
-  printf("Valor do ultimo elemenbto  : %d\n", pilha1->dado);
+  // printf("O tamanho da pilha1 eh: %d\n", contarNos(pilha1));
+  gerarPilha(pilha1, 100);
+  // exibir(pilha1);
 
-  printf("O tamanho da pilha1 eh: %d\n", contarNos(pilha1));
-  exibir(pilha1);
+  // printf("O numero 12 esta ou nao na pilha? %d", localizado(12, pilha1));
 
-  printf("O numero 12 esta ou nao na pilha? %d", localizado(12, pilha1));
-
-  return 1;
+  return 0;
 }
